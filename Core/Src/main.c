08293d43ba966e5d20259924ff080eba5f1cac7a
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2026-04-02 00:53:38 +0300

    [TrafficLight] Implement

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,13 +21,21 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum {
+	GREEN,
+	GREEN_BLINK,
+	YELLOW,
+	RED,
+	RED_YELLOW,
+	YELLOW_BLINK,
+} TrafficLightState_TypeDef;
 
 /* USER CODE END PTD */
 
@@ -42,14 +50,26 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
-static volatile int is_button_pressed = 0;
-static volatile int button_pressed_count = 0;
 
-static uint8_t data[100];
-static uint16_t len;
+static const uint16_t GREEN_STATE_DELAY = 10000;
+static const uint16_t BLINK_DELAY = 500;
+static const uint16_t GREEN_STATE_BLINK_MAX_COUNT = 9;
+static const uint16_t YELLOW_STATE_DELAY = 4000;
+static const uint16_t RED_STATE_DELAY = 10000;
+static const uint16_t RED_YELLOW_STATE_DELAY = 4000;
+
+static volatile TrafficLightState_TypeDef traffic_light_state = GREEN;
+static volatile uint32_t state_current_time = 0;
+static volatile uint32_t state_green_blink_count = 0;
+
+
+static volatile uint8_t data[100];
+static volatile uint16_t len;
 
 /* USER CODE END PV */
 
@@ -57,6 +77,7 @@ static uint16_t len;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -96,11 +117,16 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+
+  HAL_TIM_Base_Start(&htim2);
 
   sprintf((char *) data, "MCU is initialized\n");
   len = strlen((char *) data);
   HAL_UART_Transmit(&huart1, data, len, 100);
+
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET);
 
   /* USER CODE END 2 */
 
@@ -111,16 +137,89 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  GPIO_PinState pin_value = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_3);
 
-	  if (is_button_pressed == 1 && pin_value == GPIO_PIN_RESET)
-	  {
-		  button_pressed_count++;
-		  is_button_pressed = 0;
+	  uint32_t current_clock = __HAL_TIM_GET_COUNTER(&htim2);
 
-		  sprintf((char *) data, "Button is pressed! General count: %d\n", button_pressed_count);
-		  len = strlen((char *) data);
-		  HAL_UART_Transmit(&huart1, data, len, 100);
+	  GPIO_PinState button_state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_1);
+
+	  if (button_state == GPIO_PIN_RESET) {
+		  if (traffic_light_state == YELLOW_BLINK) {
+			  traffic_light_state = RED;
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+			  state_current_time = current_clock;
+		  } else {
+			  traffic_light_state = YELLOW_BLINK;
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_RESET);
+			  state_current_time = current_clock;
+		  }
+	  }
+
+	  if (
+			  traffic_light_state == GREEN &&
+			  current_clock - state_current_time >= GREEN_STATE_DELAY
+	  ) {
+		  traffic_light_state = GREEN_BLINK;
+		  state_current_time = current_clock;
+	  }
+
+	  if (
+			  traffic_light_state == GREEN_BLINK &&
+			  current_clock - state_current_time >= BLINK_DELAY
+	  ) {
+		  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_3);
+		  state_green_blink_count++;
+		  state_current_time = current_clock;
+	  }
+
+	  if (
+			  traffic_light_state == YELLOW_BLINK &&
+			  current_clock - state_current_time >= BLINK_DELAY
+	  ) {
+		  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_0);
+		  state_current_time = current_clock;
+	  }
+
+	  if (
+			  traffic_light_state == GREEN_BLINK &&
+			  state_green_blink_count == GREEN_STATE_BLINK_MAX_COUNT
+	  ) {
+		  traffic_light_state = YELLOW;
+		  state_green_blink_count = 0;
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+		  state_current_time = current_clock;
+	  }
+
+	  if (
+			  traffic_light_state == YELLOW &&
+			  current_clock - state_current_time >= YELLOW_STATE_DELAY
+	  ) {
+		  traffic_light_state = RED;
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
+		  state_current_time = current_clock;
+	  }
+
+	  if (
+			  traffic_light_state == RED &&
+			  current_clock - state_current_time >= RED_STATE_DELAY
+	  ) {
+		  traffic_light_state = RED_YELLOW;
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_SET);
+		  state_current_time = current_clock;
+	  }
+
+	  if (
+			  traffic_light_state == RED_YELLOW &&
+			  current_clock - state_current_time >= RED_YELLOW_STATE_DELAY
+	  ) {
+		  traffic_light_state = GREEN;
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET);
+		  state_current_time = current_clock;
 	  }
   }
   /* USER CODE END 3 */
@@ -139,12 +238,11 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -157,10 +255,10 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -170,6 +268,65 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 36000 - 1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4294967295;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_OC_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_TIMING;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_OC_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
 }
 
 /**
@@ -222,16 +379,23 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /*Configure GPIO pin : PC3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PC0 PC2 PC3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+  /*Configure GPIO pin : PC1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
@@ -239,14 +403,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-    if(GPIO_Pin == GPIO_PIN_3)
-    {
-    	is_button_pressed = 1;
-    }
-}
 
 /* USER CODE END 4 */
 
