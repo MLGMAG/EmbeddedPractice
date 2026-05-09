6a5c7ed49d5846408f92b02ae2fcb686e97e5d72
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2026-05-09 23:53:31 +0300

    [Module_4.3] Implement

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,10 @@
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
+#include "BME280.h"
+#include "ssd1306.h"
+#include "ssd1306_fonts.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,25 +45,117 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim1;
+
 UART_HandleTypeDef huart1;
+DMA_HandleTypeDef hdma_usart1_tx;
 
 /* USER CODE BEGIN PV */
 
-static uint8_t data[100];
-static uint16_t len;
+static volatile uint8_t tx_available = 1;
+static volatile uint8_t tim1_interrupt = 0;
+
+BME280_Data_t BME280;
+
+static uint8_t message_buffer[100];
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void BME280_Init(void);
+static HAL_StatusTypeDef UART_MessageTransmit(const uint8_t*);
+static void SSD1306_UpdateScreen(BME280_Data_t*);
+static HAL_StatusTypeDef UART_DebugLogTransit(BME280_Data_t*);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static void BME280_Init(void)
+{
+
+  //Init structure definition section
+	BME280_Init_t BME280_InitStruct = {0};
+
+	//Reset section
+	Reset_BME280();
+
+	/*============================ *BME280 Initialization* ============================*/
+
+	BME280_InitStruct.Filter = FILTER_8;     				//FILTER_X
+	BME280_InitStruct.Mode = BME280_NORMAL_MODE;		 	//SLEEP, NORMAL or FORCE can be written
+	BME280_InitStruct.OverSampling_H = OVERSAMPLING_16;		//OVERSAMPLING_X
+	BME280_InitStruct.OverSampling_P = OVERSAMPLING_16;		//OVERSAMPLING_X
+	BME280_InitStruct.OverSampling_T = OVERSAMPLING_16;		//OVERSAMPLING_X
+	BME280_InitStruct.SPI_EnOrDıs = SPI3_W_DISABLE;			//SPI3_W_DISABLE or SPI3_W_ENABLE can be written
+	BME280_InitStruct.T_StandBy = T_SB_250;					//T_SB_X
+
+	BME280Init(BME280_InitStruct);
+}
+
+static HAL_StatusTypeDef UART_MessageTransmit(const uint8_t *message) {
+	HAL_StatusTypeDef result = HAL_BUSY;
+	if (tx_available == 1) {
+		tx_available = 0;
+
+		uint16_t len = strlen((char *) message);
+		result = HAL_UART_Transmit_DMA(&huart1, message, len);
+
+		if (result != HAL_OK) {
+			tx_available = 1;
+		}
+	}
+	return result;
+}
+
+static void SSD1306_UpdateScreen(BME280_Data_t* sensor_data) {
+	  ssd1306_SetCursor(5, 5);
+	  ssd1306_WriteString("Maga loves Kate!", Font_6x8, White);
+
+	  sprintf(
+			  (char *) message_buffer,
+			  "T: %.1f°C",
+			  sensor_data->Temperature
+	  );
+	  ssd1306_SetCursor(5, 20);
+	  ssd1306_WriteString(message_buffer, Font_6x8, White);
+
+	  sprintf(
+			  (char *) message_buffer,
+			  "RH: %d%%",
+			  ((int) sensor_data->Humidity)
+	  );
+	  ssd1306_SetCursor(5, 35);
+	  ssd1306_WriteString(message_buffer, Font_6x8, White);
+
+	  sprintf(
+			  (char *) message_buffer,
+			  "P: %d hPa",
+			  ((int) sensor_data->Pressure)
+	  );
+	  ssd1306_SetCursor(5, 50);
+	  ssd1306_WriteString(message_buffer, Font_6x8, White);
+
+	  ssd1306_UpdateScreen();
+}
+
+static HAL_StatusTypeDef UART_DebugLogTransit(BME280_Data_t* sensor_data) {
+	  sprintf(
+			  (char *) message_buffer,
+			  "T: %.1f°C  RH: %d%%  P: %d hPa\n",
+			  sensor_data->Temperature, ((int) sensor_data->Humidity), ((int) sensor_data->Pressure)
+	  );
+
+	  return UART_MessageTransmit(message_buffer);
+}
 
 /* USER CODE END 0 */
 
@@ -92,12 +188,18 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
 
-  sprintf((char *) data, "MCU is initialized\n");
-  len = strlen((char *) data);
-  HAL_UART_Transmit(&huart1, data, len, 100);
+  UART_MessageTransmit("MCU_INIT\n");
+  HAL_TIM_Base_Start_IT(&htim1);
+
+  BME280_Init();
+
+  ssd1306_Init();
 
   /* USER CODE END 2 */
 
@@ -108,6 +210,15 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	  if (tim1_interrupt == 1) {
+		  BME280Calculation(&BME280);
+		  SSD1306_UpdateScreen(&BME280);
+		  UART_DebugLogTransit(&BME280);
+
+		  HAL_TIM_Base_Start_IT(&htim1);
+
+		  tim1_interrupt = 0;
+	  }
   }
   /* USER CODE END 3 */
 }
@@ -154,6 +265,86 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 16000 - 1;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 1000 - 1;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
@@ -187,6 +378,22 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -199,6 +406,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
@@ -206,7 +414,20 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart == &huart1)
+	{
+		tx_available = 1;
+	}
+}
 
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	if (htim == &htim1)
+	{
+		tim1_interrupt = 1;
+		HAL_TIM_Base_Stop_IT(&htim1);
+	}
+}
 /* USER CODE END 4 */
 
 /**
