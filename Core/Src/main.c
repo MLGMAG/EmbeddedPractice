dddbf04a7c3b7f89ee0ac7633adff067729d07b1
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2026-06-26 12:10:32 +0300

    [FreeRtosQueues] Implement

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,38 +1,45 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
+#include <projdefs.h>
+#include "ssd1306.h"
+#include "ssd1306_fonts.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+typedef struct {
+	float adc_data;
+} MSGQUEUE_OBJ_t;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define MSGQUEUE_OBJECTS 4
+#define GENERAL_TASK_DELAY 100
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -41,26 +48,94 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim2;
+
 UART_HandleTypeDef huart1;
 
+/* Definitions for task1 */
+osThreadId_t task1Handle;
+const osThreadAttr_t task1_attributes = {
+  .name = "task1",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for task2 */
+osThreadId_t task2Handle;
+const osThreadAttr_t task2_attributes = {
+  .name = "task2",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for task3 */
+osThreadId_t task3Handle;
+const osThreadAttr_t task3_attributes = {
+  .name = "task3",
+  .stack_size = 256 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
 /* USER CODE BEGIN PV */
-
 static uint8_t data[100];
-static uint16_t len;
+static uint8_t data_len;
+static uint8_t screen_message_buffer[100];
 
+static osMessageQueueId_t adc_uart_MsgQueue;
+static osMessageQueueId_t adc_screen_MsgQueue;
+
+static volatile uint16_t adc[2] = { 0, };
+static volatile uint8_t dma_completion_flag = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
-/* USER CODE BEGIN PFP */
+static void MX_I2C1_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM2_Init(void);
+void start_task1(void *argument);
+void start_task2(void *argument);
+void start_task3(void *argument);
 
+/* USER CODE BEGIN PFP */
+static void SSD1306_Screen_Update(const float*);
+static float ADC_Value_Convert(const volatile uint16_t*);
+static void MessageQueue_Status_Check(const osStatus_t*, const char*);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static void SSD1306_Screen_Update(const float *adc_value) {
+	ssd1306_Fill(Black);
 
+	ssd1306_SetCursor(5, 5);
+	ssd1306_WriteString("Maga loves Kate!", Font_6x8, White);
+
+	sprintf((char*) screen_message_buffer, "ADC value: %.2f", *adc_value);
+	ssd1306_SetCursor(5, 20);
+	ssd1306_WriteString((char*) screen_message_buffer, Font_6x8, White);
+
+	ssd1306_UpdateScreen();
+}
+
+static float ADC_Value_Convert(const volatile uint16_t *adc_value) {
+	return ((float) *adc_value) / 4095 * 3.3;
+}
+
+static void MessageQueue_Status_Check(const osStatus_t *status,
+		const char *task_details) {
+	if (*status != osOK) {
+		sprintf((char*) data, "Queue task '%s' is failed with code %d\n",
+				task_details, *status);
+		data_len = strlen((char*) data);
+		HAL_UART_Transmit(&huart1, data, data_len, 100);
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -92,23 +167,76 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+	ssd1306_Init();
 
-  sprintf((char *) data, "MCU is initialized\n");
-  len = strlen((char *) data);
-  HAL_UART_Transmit(&huart1, data, len, 100);
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*) &adc, 2);
+	HAL_TIM_Base_Start(&htim2);
 
+	sprintf((char*) data, "MCU is initialized\n");
+	data_len = strlen((char*) data);
+	HAL_UART_Transmit(&huart1, data, data_len, 100);
   /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+	adc_uart_MsgQueue = osMessageQueueNew(MSGQUEUE_OBJECTS,
+			sizeof(MSGQUEUE_OBJ_t),
+			NULL);
+	adc_screen_MsgQueue = osMessageQueueNew(MSGQUEUE_OBJECTS,
+			sizeof(MSGQUEUE_OBJ_t),
+			NULL);
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of task1 */
+  task1Handle = osThreadNew(start_task1, NULL, &task1_attributes);
+
+  /* creation of task2 */
+  task2Handle = osThreadNew(start_task2, NULL, &task2_attributes);
+
+  /* creation of task3 */
+  task3Handle = osThreadNew(start_task3, NULL, &task3_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+	}
   /* USER CODE END 3 */
 }
 
@@ -132,7 +260,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -142,15 +275,163 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+  ADC_InjectionConfTypeDef sConfigInjected = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+  hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T2_TRGO;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = ENABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configures for the selected ADC injected channel its corresponding rank in the sequencer and its sample time
+  */
+  sConfigInjected.InjectedChannel = ADC_CHANNEL_0;
+  sConfigInjected.InjectedRank = 1;
+  sConfigInjected.InjectedNbrOfConversion = 1;
+  sConfigInjected.InjectedSamplingTime = ADC_SAMPLETIME_3CYCLES;
+  sConfigInjected.ExternalTrigInjecConvEdge = ADC_EXTERNALTRIGINJECCONVEDGE_NONE;
+  sConfigInjected.ExternalTrigInjecConv = ADC_INJECTED_SOFTWARE_START;
+  sConfigInjected.AutoInjectedConv = DISABLE;
+  sConfigInjected.InjectedDiscontinuousConvMode = DISABLE;
+  sConfigInjected.InjectedOffset = 0;
+  if (HAL_ADCEx_InjectedConfigChannel(&hadc1, &sConfigInjected) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 1000 - 1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 100000 - 1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_ENABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
 }
 
 /**
@@ -187,6 +468,22 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA2_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -199,6 +496,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
@@ -206,8 +504,109 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+	if (hadc->Instance == ADC1) {
+		dma_completion_flag = 1;
+	}
+}
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_start_task1 */
+/**
+ * @brief  Function implementing the task1 thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_start_task1 */
+void start_task1(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+	MSGQUEUE_OBJ_t msg;
+	osStatus_t status;
+	uint32_t putMessage_delay = pdMS_TO_TICKS(1000);
+	const char *uart_task_details = "send ADC message to UART";
+	const char *screen_task_details = "send ADC message to UART";
+
+	/* Infinite loop */
+	while (1) {
+		if (dma_completion_flag == 1) {
+			msg.adc_data = (ADC_Value_Convert(&adc[0]) + ADC_Value_Convert(&adc[1])) / 2;
+
+			status = osMessageQueuePut(adc_uart_MsgQueue, &msg, 0U,
+					putMessage_delay);
+			MessageQueue_Status_Check(&status, uart_task_details);
+
+			status = osMessageQueuePut(adc_screen_MsgQueue, &msg, 0U,
+					putMessage_delay);
+			MessageQueue_Status_Check(&status, screen_task_details);
+
+			dma_completion_flag = 0;
+		}
+
+		osDelay(pdMS_TO_TICKS(GENERAL_TASK_DELAY));
+	}
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_start_task2 */
+/**
+ * @brief Function implementing the task2 thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_start_task2 */
+void start_task2(void *argument)
+{
+  /* USER CODE BEGIN start_task2 */
+	/* Infinite loop */
+	MSGQUEUE_OBJ_t msg;
+	osStatus_t status;
+	uint32_t getMessage_delay = pdMS_TO_TICKS(10000);
+	const char *task_details = "Receive ADC message UART task";
+
+	while (1) {
+		status = osMessageQueueGet(adc_uart_MsgQueue, &msg, NULL,
+				getMessage_delay);
+		if (status == osOK) {
+			sprintf((char*) data, "ADC value: %.2f\n", msg.adc_data);
+			data_len = strlen((char*) data);
+			HAL_UART_Transmit(&huart1, data, data_len, 100);
+		} else {
+			MessageQueue_Status_Check(&status, task_details);
+		}
+		osDelay(pdMS_TO_TICKS(GENERAL_TASK_DELAY));
+	}
+  /* USER CODE END start_task2 */
+}
+
+/* USER CODE BEGIN Header_start_task3 */
+/**
+ * @brief Function implementing the task3 thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_start_task3 */
+void start_task3(void *argument)
+{
+  /* USER CODE BEGIN start_task3 */
+	/* Infinite loop */
+	MSGQUEUE_OBJ_t msg;
+	osStatus_t status;
+	uint32_t getMessage_delay = pdMS_TO_TICKS(10000);
+	const char *task_details = "Receive ADC message SCREEN task";
+
+	while (1) {
+		status = osMessageQueueGet(adc_screen_MsgQueue, &msg, NULL,
+				getMessage_delay);
+		if (status == osOK) {
+			SSD1306_Screen_Update(&msg.adc_data);
+		} else {
+			MessageQueue_Status_Check(&status, task_details);
+		}
+		osDelay(pdMS_TO_TICKS(GENERAL_TASK_DELAY));
+	}
+  /* USER CODE END start_task3 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -216,11 +615,10 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
   /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
