bb320150c19b0108d7f1158c45fb4f72f7deb133
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2026-09-06 15:18:18 +0300

    [SensorsDataSyncTask] Make sync interval configurable

--- main/Src/task/sensors_data_sync_task.c
+++ main/Src/task/sensors_data_sync_task.c
@@ -1,12 +1,13 @@
 #include "task/sensors_data_sync_task.h"
 #include "esp_log.h"
 #include "freertos/idf_additions.h"
+#include "sdkconfig.h"
 
 static const char *TAG = "SENSORS_DATA_SYNC_TASK";
 
 void UAL_SENSORS_DATA_SYNC_TASK_Start(void *pvParameters) {
 	while (1) {
 		ESP_LOGI(TAG, "Synchronize sensors data.");
-		vTaskDelay(pdMS_TO_TICKS(60000));
+		vTaskDelay(pdMS_TO_TICKS(CONFIG_SENSORS_DATA_SYNC_TIME_MS));
 	}
 }
